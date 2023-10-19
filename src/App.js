03fd05d52f6834a1722bff0b34096bfd1c24c6b5
ovovvvvv <ovovvvvv@naver.com>
2023-10-19 12:48:42 +0900

    중첩 라우트, id부분이 동적으로 변하는것 확인

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Profile from "./pages/Profile";
 import "./App.css";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/profiles/:username" element={<Profile />} />
+      <Route path="/articles" element={<Articles />} />
+      <Route path="/articles/:id" element={<Article />} />
     </Routes>
   );
 }
