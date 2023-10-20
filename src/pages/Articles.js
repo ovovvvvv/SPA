c569561aaff5eb1bb6393f88cd7552a6ab6ad86c
ovovvvvv <ovovvvvv@naver.com>
2023-10-20 14:47:43 +0900

    NavLink사용

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -1,19 +1,39 @@
 import React from "react";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 const Articles = () => {
+  const activeStyle = {
+    color: "green",
+    fontSize: 21,
+  };
   return (
     <div>
       <Outlet />
       <ul>
         <li>
-          <Link to="/articles/1">게시글 1</Link>
+          <NavLink
+            to="/articles/1"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+         // isActive가 true이면 activeStyle 객체를 반환하여 해당 링크에 activeStyle을 적용 false이면 undefined반환
+          >
+            게시글 1
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/2">게시글 2</Link>
+          <NavLink
+            to="/articles/2"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 2
+          </NavLink>
         </li>
         <li>
-          <Link to="/articles/3">게시글 3</Link>
+          <NavLink
+            to="/articles/3"
+            style={({ isActive }) => (isActive ? activeStyle : undefined)}
+          >
+            게시글 3
+          </NavLink>
         </li>
       </ul>
     </div>
