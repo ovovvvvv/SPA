482d7e05bd572688fbb1b958a9b3c2bdbc73d9ca
ovovvvvv <ovovvvvv@naver.com>
2023-10-20 14:41:49 +0900

    useNavigate, replace 옵션

--- src/Layout.js
+++ src/Layout.js
@@ -1,11 +1,23 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 
 const Layout = () => {
+  const navigate = useNavigate();
+
+  const goBack = () => {
+    // 이전 페이지로 이동
+    navigate(-1);
+  };
+
+  const goArticles = () => {
+    // 페이지 목록으로 이동
+    navigate("./articles", { replace: true });
+  };
   return (
     <div>
       <header style={{ background: "lightgray", padding: 16, fontSize: 24 }}>
-        Header
+        <button onClick={goBack}>뒤로가기</button>
+        <button onClick={goArticles}> 게시글 목록 </button>
       </header>
       <main>
         <Outlet />
