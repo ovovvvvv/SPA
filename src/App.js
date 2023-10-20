4bd66ca53b4b6a42f49b4162af49bf0cf9ae96a7
ovovvvvv <ovovvvvv@naver.com>
2023-10-20 15:16:04 +0900

    NotFound 페이지

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
 import Layout from "./Layout";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -18,8 +19,12 @@ function App() {
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
 }
 
+/* 여기서 *은 wildcard의 문자로, 아무 텍스트나 매칭한다는 뜻이다.
+ 이 라우트 엘리먼트의 상단에 위치하는 라우트들의 규칙을 모두 확인하고, 
+ 일치하는 라우트가 없다면 이 라우트가 화면에 나타나게 된다. */
 export default App;
