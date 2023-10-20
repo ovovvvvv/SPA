52483581fa47de4da4d394e0fc8066932a66892f
ovovvvvv <ovovvvvv@naver.com>
2023-10-20 14:09:50 +0900

    공통 레이아웃 컴포넌트git add .

--- src/App.js
+++ src/App.js
@@ -5,13 +5,16 @@ import Profile from "./pages/Profile";
 import "./App.css";
 import Articles from "./pages/Articles";
 import Article from "./pages/Article";
+import Layout from "./Layout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/profiles/:username" element={<Profile />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/profiles/:username" element={<Profile />} />
+      </Route>
       <Route path="/articles" element={<Articles />}>
         <Route path=":id" element={<Article />} />
       </Route>
